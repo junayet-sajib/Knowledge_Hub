a806c77400a0c0f57cde31f4be39659130516514
Junayet Sajib <junayet3@gmail.com>
2024-03-15 15:44:49 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Blogs from './componets/Blogs/Blogs'
+import Header from './componets/Header/Header'
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+      <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
